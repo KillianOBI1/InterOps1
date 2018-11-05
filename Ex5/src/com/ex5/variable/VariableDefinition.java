a8674163bc6c182c30ed43be55c3c444e9bf5281
KillianOBI1 <killian.monot@orange.fr>
2018-11-05 14:32:37 +0100

    modif class

--- Ex5/src/com/ex5/variable/VariableDefinition.java
+++ Ex5/src/com/ex5/variable/VariableDefinition.java
@@ -1,14 +1,19 @@
 package com.ex5.variable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VariableDefinition {
   public String name;
   public String type;
   public String visibility;
+  List<VariableReference> listReference;
   
   public VariableDefinition(String name,String type,String visibility) {
 	  this.name = name;
 	  this.type = type;
 	  this.visibility = visibility;
+	  this.listReference = new ArrayList<VariableReference>();
   }
   
   public VariableDefinition() {
--- Ex5/src/com/ex5/variable/VariableReference.java
+++ Ex5/src/com/ex5/variable/VariableReference.java
@@ -1,14 +1,22 @@
 package com.ex5.variable;
 
 public class VariableReference extends Reference {
-  public VariableDefinition variableDefinition;
+  protected VariableDefinition variableDefinition;
   
   public VariableReference() {
 	  super();
-	  this.variableDefinition = new VariableDefinition();
   }
   
   public void setName(String name) {
 	  this.name = name;
+  }
+  
+  
+  public void setDefinition(VariableDefinition variableDefinition) {
+	  this.variableDefinition = variableDefinition;
+  }
+  
+  public VariableDefinition getVariableDefinition() {
+	return this.variableDefinition;
   }
 }
